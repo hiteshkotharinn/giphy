39aad8dca99fa2be399d433e18f07de9af75428a
hitesh_kothari <hitesh.kothari@paptronics.com>
2021-06-04 22:01:18 +0530

    giphy-search-form ui design and route configuration complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GiphyComponent } from './giphy/giphy.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', 'component': GiphyComponent },
+  { path: '**', 'component': NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
